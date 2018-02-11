19112c159a078631c6c5e588405fd7e4dbfb64b2
zhongkai <kaizhongfan@163.com>
2018-02-11 18:14:50 +0800

    by stefan

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const lessonConfig = require('./lesson');
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>');
+    res.send(`<h1>Hello World! This Class is called '${ lessonConfig.lesson }'. Teacher is ${ lessonConfig.teacher }.</h1>`);
 });
 
 app.listen(port, error => {
