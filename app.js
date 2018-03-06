78e8783e1a3a54b100071d76732ee8b534cfba7c
zhongkai <kaizhongfan@163.com>
2018-03-06 14:32:42 +0800

    by stefan

--- app.js
+++ app.js
@@ -21,6 +21,23 @@ app.use('/user', user);
 
 app.use('/todos', todos);
 
+app.use(function(req, res, next) {
+
+    res.status(404).json({
+        error: '资源未找到'
+    });
+
+});
+
+app.use(function(error, req, res, next) {
+
+    console.log(error);
+    res.status(500).json({
+        error: '服务器内部错误'
+    });
+
+});
+
 app.listen(port, function(error) {
     if(error) {
         console.log('error!');
